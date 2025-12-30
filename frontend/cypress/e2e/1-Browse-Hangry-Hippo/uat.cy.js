1610bcac70c2b3baf13318f4c0bcad06e1783bf2
Titus Muthomi <muthomititus+github@gmail.com>
2025-12-30 15:27:44 +0300

    Automate acceptance tests

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -3,9 +3,17 @@ describe('Shows Appeteasers', () => {
     cy.visit('https://hangryhippo.quantic.host/');
   });
   it('selects Handhelds', () => {
+    cy.contains('Handhelds').click();
+    cy.contains('Cheese Burger');
+    cy.contains('Fajita Tacos');
     //TODO: Add a test to check Handhelds
   });
   it('selects Appeteasers', () => {
+    cy.contains('Appeteasers').click();
+    cy.contains('Tater Tots');
+    cy.contains('Buffalo Wings');
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
     //TODO: Add a test to check Appeteasers
   });
 });
